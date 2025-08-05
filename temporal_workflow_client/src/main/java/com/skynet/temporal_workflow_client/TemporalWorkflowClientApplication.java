94a80fbde3c8d8ba34488317cb9591e026550ed9
borrinim <mattia.borrini@post.ch>
2025-08-05 14:00:56 +0200

    Add temporal workflow client and docker

---
+++ temporal_workflow_client/src/main/java/com/skynet/temporal_workflow_client/TemporalWorkflowClientApplication.java
@@ -0,0 +1,13 @@
+package com.skynet.temporal_workflow_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalWorkflowClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalWorkflowClientApplication.class, args);
+	}
+
+}
---
+++ temporal_workflow_client/src/test/java/com/skynet/temporal_workflow_client/TemporalWorkflowClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skynet.temporal_workflow_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalWorkflowClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
